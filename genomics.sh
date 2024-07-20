cc01900c9898c4a20f55e309ad059c886ff0d27a
igorkf <igorkuivjogi@hotmail.com>
2024-07-19 22:23:57 -0500

    add initial genomics pipeline

---
+++ genomics.sh
@@ -0,0 +1,11 @@
+# load modules
+# module load samtools/1.10 gatk/4.2.6.1
+
+# filter samples
+bcftools view -S output/geno_samples.txt data/chr_1_imputed.vcf.gz > output/chr_1_imputed_filt.vcf.gz
+
+# keep only snps and MAF > 0.01
+bcftools view -m2 -M2 -v snps -q 0.01:minor output/chr_1_imputed_filt.vcf.gz > output/chr_1_imputed_filt_m001_snps.vcf.gz
+
+# LD pruning and missing filtering
+bcftools +prune -w 100 -l 0.9 -e'F_MISSING>=0.5' output/chr_1_imputed_filt_m001_snps.vcf.gz -Oz -o output/chr_1_imputed_filt_m001_snps_pruned.vcf.gz
